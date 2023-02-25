f8ded98bc709b44f7df572d7a473d7ed7a790470
Givago <givago.fritzen@hotmail.com>
2023-02-25 19:17:49 -0300

    Header scroll to div

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { HeaderAnimation } from '../../utils/animations/header-animation';
 
@@ -8,13 +9,20 @@ import { HeaderAnimation } from '../../utils/animations/header-animation';
   animations: [HeaderAnimation],
 })
 export class HeaderComponent {
-  show = true;
+  show = false;
   currentPosition: number = 0;
+
+  constructor(private scroller: ViewportScroller) { }
+
+  goToId(id: string) {
+    this.scroller.scrollToAnchor(id);
+  }
+
 
   @HostListener('window:scroll')
   onWindowScroll() {
     let scroll = window.pageYOffset;
-    if (scroll > this.currentPosition) {
+    if (scroll > this.currentPosition || this.currentPosition == 0) {
       this.show = true;
     } else {
       this.show = false;
