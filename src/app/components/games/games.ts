4d4e2a348a5b52758b0e35617f457298d698627c
Givago <givago.fritzen@hotmail.com>
2023-04-28 15:07:27 -0300

    Ajuste Modal Game

--- src/app/components/games/games.ts
+++ src/app/components/games/games.ts
@@ -6,7 +6,7 @@ export const slides: GameModel[] = [
         imageSrc: 'bearinc', src: 'https://givagofritzen.itch.io/bear-inc', title: 'Bearinc', text: 'bearinc'
     },
     {
-        imageSrc: 'blindway', src: 'https://givagofritzen.itch.io/blind-way', title: '', text: 'blindway'
+        imageSrc: 'blindway', src: 'https://givagofritzen.itch.io/blind-way', title: 'Blindway', text: 'blindway'
     },
     {
         imageSrc: 'trash', src: 'https://givagofritzen.itch.io/trash', title: 'Trash', text: 'trash'
@@ -15,7 +15,7 @@ export const slides: GameModel[] = [
         imageSrc: 'war', src: 'https://givagofritzen.itch.io/war', title: 'War', text: 'war'
     },
     {
-        imageSrc: 'wave-makers', src: 'https://globalgamejam.org/2017/games/wavemakers', title: '', text: 'wave-makers'
+        imageSrc: 'wave-makers', src: 'https://globalgamejam.org/2017/games/wavemakers', title: 'Wave Makers', text: 'wave-makers'
     },
     {
         imageSrc: 'zombies-kill', src: 'https://givagofritzen.itch.io/zombieskill', title: 'ZombiesKill', text: 'zombies-kill'
@@ -30,10 +30,10 @@ export const slides: GameModel[] = [
         imageSrc: 'crazy-damas', src: 'https://givagofritzen.itch.io/damas-crazy', title: 'Crazy Damas', text: 'damas-crazy'
     },
     {
-        imageSrc: 'death-poa', src: 'https://reversalmirror.itch.io/death-poa', title: '', text: 'death-poa'
+        imageSrc: 'death-poa', src: 'https://reversalmirror.itch.io/death-poa', title: 'Death Poa', text: 'death-poa'
     },
     {
-        imageSrc: 'into-the-sumbler', src: 'https://ldjam.com/events/ludum-dare/40/into-the-slumber', title: '', text: 'into-the-slumber'
+        imageSrc: 'into-the-sumbler', src: 'https://ldjam.com/events/ludum-dare/40/into-the-slumber', title: 'Into The Slumber', text: 'into-the-slumber'
     },
     {
         imageSrc: 'salvor-2289', src: 'https://www.zoydgames.com/salvor', title: 'Salvor 2289', text: 'salvor'
@@ -42,7 +42,7 @@ export const slides: GameModel[] = [
         imageSrc: 'light', src: 'https://globalgamejam.org/2018/games/light-0', title: 'Light', text: 'light'
     },
     {
-        imageSrc: 'inside-self', src: 'https://globalgamejam.org/2019/games/inside-self', title: '', text: 'inside-self'
+        imageSrc: 'inside-self', src: 'https://globalgamejam.org/2019/games/inside-self', title: 'Inside Self', text: 'inside-self'
     },
     {
         imageSrc: 'random-keys', src: 'https://givagofritzen.itch.io/random-keys', title: 'Random Keys', text: 'random-keys'
